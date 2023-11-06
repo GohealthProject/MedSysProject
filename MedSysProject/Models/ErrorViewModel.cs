af7bc03e3a30c93c57f8e3582dca9047c2b850df
順 陳 <night90810@gmail.com>
2023-11-06 15:19:41 +0800

    加入專案檔案。

---
+++ MedSysProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MedSysProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
