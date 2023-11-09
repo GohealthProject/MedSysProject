cee1a9c30aade6ad184d1bb30b6f30b128be69be
Jacky Jie <jakcy2222@gmail.com>
2023-11-09 19:56:51 +0800

    把前端頁面調整好了，並且搬到HomeController去

--- MedSysProject/Program.cs
+++ MedSysProject/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Test}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
