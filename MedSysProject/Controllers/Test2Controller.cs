3a0df9748569da6e395c4d2b38e3ad017480a2ca
家蜜 Ms <s434780@hotmail.com>
2023-11-09 17:40:26 +0800

    前端html首頁已設定，Views裡的_Layout_Fore，Test裡的index。

---
+++ MedSysProject/Controllers/Test2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedSysProject.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
