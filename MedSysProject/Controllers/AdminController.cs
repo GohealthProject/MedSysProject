1d02bb89466252da12f6d21c58a89669146a15db
Jacky Jie <jakcy2222@gmail.com>
2023-11-10 17:05:13 +0800

    嘗試搞登入 尚未完成

--- MedSysProject/Controllers/AdminController.cs
+++ MedSysProject/Controllers/AdminController.cs
@@ -1,10 +1,12 @@
 ﻿using MedSysProject.Models;
+using MedSysProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedSysProject.Controllers
 {
     public class AdminController : Controller
     {
+
         public IActionResult Service()
         {
             
@@ -21,5 +23,26 @@ namespace MedSysProject.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Login(CLoginViewModel vm)
+        {
+            Employee emp = new Employee();
+
+            // 假設 EmployeeClass 具有一個名為 Employees 的屬性，它是 IQueryable<Employee> 的類型
+
+            var user = from i in emp.EmployeeEmail
+                       select i;
+
+            if (user != null)
+            {
+                if (vm.txtPassWord == user)
+                {
+                    return RedirectToAction("Index");
+                }
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
--- MedSysProject/ViewModels/CLoginViewModel.cs
+++ MedSysProject/ViewModels/CLoginViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace MedSysProject.ViewModels
+﻿using MedSysProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedSysProject.ViewModels
 {
     public class CLoginViewModel
     {
         public string txtEmail { get; set; }
         public string txtPassWord { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
     }
 }
