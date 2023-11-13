c5fd333249c580c806a9948c9e4ef4fce7b61288
順 陳 <night90810@gmail.com>
2023-11-13 16:47:42 +0800

    Login 第四十四行可以調整寬度

--- MedSysProject/Program.cs
+++ MedSysProject/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Shopping}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Login}/{id?}");
 
 app.Run();
