a291355e86499bc8e25bc28abed61c4976ca5c22
SteveisComing <emile814034@gmail.com>
2023-11-15 15:41:33 +0800

    要有重新來過的勇氣 恩

---
+++ MedSysProject/Controllers/BlogsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedSysProject.Controllers
+{
+    public class BlogsController : Controller
+    {
+        /// <summary>
+        /// 主畫面
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SinglePost() 
+        {
+            return View();
+        }
+
+
+    }
+}
