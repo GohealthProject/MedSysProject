68fc0a7f3557d040c2b944834f40e873c2b8b960
SteveisComing <136465286+SteveisComing@users.noreply.github.com>
2023-11-19 14:36:48 +0800

    StickyHeader效果

---
+++ MedSysProject/wwwroot/assets/js/mine.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    /*sticky header效果*/
+    const header = document.querySelector('#header');
+    console.log(header);
+    if (header) {
+        document.addEventListener('scroll', () => {
+            window.scrollY > 100 ? header.classList.add('sticked') : header.classList.remove('sticked');
+        });
+    }
+
+});
