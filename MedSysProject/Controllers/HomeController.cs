b50d3a0cdfe374a7155b519ccb6051727363da78
家蜜 Ms <s434780@hotmail.com>
2023-11-20 11:11:57 +0800

    刪除改版

--- MedSysProject/Controllers/HomeController.cs
+++ MedSysProject/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace MedSysProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+    
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
