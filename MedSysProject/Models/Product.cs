8dd599ba572148eb8adb23cb0ed890da449c8153
順 陳 <night90810@gmail.com>
2023-11-20 16:34:41 +0800

    照片出來了

--- MedSysProject/Models/Product.cs
+++ MedSysProject/Models/Product.cs
@@ -19,8 +19,6 @@ public partial class Product
 
     public string Description { get; set; }
 
-    public byte[] Photo { get; set; }
-
     public int? UnitsInStock { get; set; }
 
     public bool? Discontinued { get; set; }
