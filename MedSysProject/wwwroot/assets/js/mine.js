d34be8b89e248cae804c4788f5e1132ef4a744d2
SteveisComing <emile814034@gmail.com>
2023-11-20 16:13:13 +0800

    安裝文字編輯器

--- MedSysProject/wwwroot/assets/js/mine.js
+++ MedSysProject/wwwroot/assets/js/mine.js
@@ -23,4 +23,49 @@
         mobileNavToggleBtn.classList.toggle('bi-list');
         mobileNavToggleBtn.classList.toggle('bi-x');
     }
+    /*不太懂 hash是什麼?*/
+    document.querySelectorAll('#navbar a').forEach(navbarlink => {
+        if (!navbarlink.hash) return;
+        let section = document.querySelector(navbarlink.hash);
+        if (!section) return;
+        navbarlink.addEventListener('click', () => {
+            if (document.querySelector('.mobile-nav-active')) {
+                mobileNavToggle();
+            }
+        })
+    })
+    const navDropdowns = document.querySelectorAll('.navbar .dropdown >a');
+    navDropdowns.forEach(el => {
+        el.addEventListener('click', function (event) {
+            if (document.querySelector('.mobile-nav-active')) {
+                event.preventDefault();
+                this.classList.toggle('active');
+                this.nextElementSibling.classList.toggle('dropdown-active');
+                let dropDownIndicator = this.querySelector('.dropdown-indicator');
+                dropDownIndicator.classList.toggle('bi-chevron-up');
+                dropDownIndicator.classList.toggle('bi-chevron-down');
+            }
+        })    
+    })
+    /*輪播牆*/
+    var swiper = new Swiper(".sliderFeaturedPosts", {
+        spaceBetween: 0,
+        speed: 500,
+        centeredSlides: true,
+        loop: true,
+        slideToClickedSlide: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".custom-swiper-button-next",
+            prevEl: ".custom-swiper-button-prev",
+        },
+    });
+
 });
