76e80db3339fe7e378cd7c8788dc83c85db96f7c
順 陳 <night90810@gmail.com>
2023-11-21 12:43:03 +0800

    套版完了

--- MedSysProject/Models/CCartItem.cs
+++ MedSysProject/Models/CCartItem.cs
@@ -1,11 +1,17 @@
-﻿namespace MedSysProject.Models
+﻿using System.ComponentModel;
+
+namespace MedSysProject.Models
 {
     public class CCartItem
     {
         public Product Product { get; set; }
+        [DisplayName("產品名稱")]
         public string ProductName { get; set; }
+        [DisplayName("單價")]
         public int UnitPrice { get; set; }
+        [DisplayName("產品總計")]
         public int 小計 { get; set; }
+        [DisplayName("數量")]
         public int count { get; set; }
 
     }
