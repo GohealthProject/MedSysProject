b8ceaba8b84d0c5714bee8957667c09a054c0bad
Jacky Jie <jakcy2222@gmail.com>
2023-11-28 12:19:04 +0800

    blog類別顯示(快完成)

--- MedSysProject/Controllers/BlogsController.cs
+++ MedSysProject/Controllers/BlogsController.cs
@@ -1,5 +1,6 @@
 ﻿using MedSysProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedSysProject.Controllers
 {
@@ -16,9 +17,9 @@ namespace MedSysProject.Controllers
         /// 主畫面
         /// </summary>
         /// <returns></returns>
-        public IActionResult IndexOld(int? id) 
+        public IActionResult IndexOld() 
         {
-            var q = _db.BlogCategories.Where(c => c.BlogClassId == id);
+            
 
             return View();
         }
@@ -29,10 +30,46 @@ namespace MedSysProject.Controllers
         }
         public IActionResult SelectBlogCategory(int? CategoryID) 
         {//
-            return View();
+         //var q = _db.BlogCategories.Where(c => c.BlogClassId == CategoryID);
+         //var q = _db.Blogs.Include(e => e.Employee).Include(e => e.ArticleClass.BlogCategory1).Where(c => c.ArticleClassId == CategoryID);
+            IEnumerable<Blog> q = null;
+
+            if (CategoryID == null)
+            {
+                q = _db.Blogs.Include(e => e.Employee);
+            }
+            else
+            {
+                q = _db.Blogs.Include(e => e.Employee).Where(c => c.ArticleClassId == CategoryID);
+            }
+            ViewBag.Cate = "aaa";/*(q.ToList())[0].ArticleClass.BlogCategory1;*/
+
+            return View(q);
         }
 
-        
+        public IActionResult GetBlogImageByte(int? id)
+        {
+            Blog emp = _db.Blogs.Find(id);
+            byte[]? img = emp?.BlogImage;
+
+            if (img != null)
+            {
+                return File(img, "image/jpeg");
+            }
+            return NotFound();
+        }
+
+        public IActionResult GetEmpImageByte(int? id)
+        {
+            Employee emp = _db.Employees.Find(id);
+            byte[]? img = emp?.EmployeePhoto;
+
+            if (img != null)
+            {
+                return File(img, "image/jpeg");
+            }
+            return NotFound();
+        }
 
     }
 }
