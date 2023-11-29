05b23be553382ff0bef8e99b9edc43e3027a06a5
wayne <waynewang1990@hotmail.com>
2023-11-29 17:08:57 +0800

    .

--- MedSysProject/Controllers/HomeController.cs
+++ MedSysProject/Controllers/HomeController.cs
@@ -1,13 +1,7 @@
 ﻿using MedSysProject.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using NuGet.Protocol;
 using System.Diagnostics;
-using System.Security.Cryptography;
-using System.Text;
-using System.Web;
 
 
 namespace MedSysProject.Controllers
@@ -133,75 +127,7 @@ namespace MedSysProject.Controllers
         }
 
         
-        public IActionResult payment()
-        {
-            //step1 : 網頁導入傳值到前端
-           
-                var orderId = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 20);
-            //需填入你的網址
-                var website = $"https://localhost:7203/"; 
-                var order = new Dictionary<string, string>
-    {
-        //綠界需要的參數
-
-        //必填
-        { "MerchantID",  "3002599"},//特店編號
-        { "MerchantTradeNo",  orderId},//特店訂單編號
-        { "MerchantTradeDate",  DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")},//特店交易時間
-        { "PaymentType",  "aio"},//交易類型(固定aio)
-        { "TotalAmount",  "1450"},//交易金額
-        { "TradeDesc",  "Test"},//交易描述
-        { "ItemName",  "測試商品"},//商品名稱
-        { "ReturnURL",  $"{website}/api/Ecpay/AddPayInfo"},//付款完成通知回傳網址
-        { "ChoosePayment",  "ALL"},//選擇預設付款方式
-        { "EncryptType",  "1"},//CheckMacValue加密類型
-
-        //選填
-        { "ExpireDate",  "3"},//分期
-        { "CustomField1",  ""},//自訂名稱欄位1
-        { "CustomField2",  ""},
-        { "CustomField3",  ""},
-        { "CustomField4",  ""},
-        { "OrderResultURL", $"{website}/Home/PayInfo/{orderId}"},//Client端回傳付款結果網址
-        //{ "PaymentInfoURL",  $"{website}/api/Ecpay/AddAccountInfo"},
-        //{ "ClientRedirectURL",  $"{website}/Home/AccountInfo/{orderId}"},
-        { "IgnorePayment",  "GooglePay#WebATM#CVS#BARCODE"},//隱藏付款方式
-        
-        
-        
-    };
-                //檢查碼
-                order["CheckMacValue"] = GetCheckMacValue(order);
-                return View(order);
-            }
-        ///待修
-            private string GetCheckMacValue(Dictionary<string, string> order)
-            {
-            var param = order.Keys.OrderBy(x => x).Select(key => key + "=" + order[key]).ToList();
-                var checkValue = string.Join("&", param);
-                //測試用的 HashKey
-                var hashKey = "spPjZn66i0OhqJsQ";
-                //測試用的 HashIV
-                var HashIV = "hT5OJckN45isQTTs";
-                checkValue = $"HashKey={hashKey}" + "&" + checkValue + $"&HashIV={HashIV}";
-                checkValue = HttpUtility.UrlEncode(checkValue).ToLower();
-                checkValue = GetSHA256(checkValue);
-                return checkValue.ToUpper();
-            }
-            private string GetSHA256(string value)
-            {
-                var result = new StringBuilder();
-                var sha256 = SHA256.Create();
-                var bts = Encoding.UTF8.GetBytes(value);
-                var hash = sha256.ComputeHash(bts);
-                for (int i = 0; i < hash.Length; i++)
-                {
-                    result.Append(hash[i].ToString("X2"));
-                }
-                return result.ToString();
-        }
-
-
+         
             
          
 
