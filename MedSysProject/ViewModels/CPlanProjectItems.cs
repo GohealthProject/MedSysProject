27de2c25d8b1a8a43a9b12aba72ef330edf3f59f
家蜜 Ms <s434780@hotmail.com>
2023-12-08 17:40:54 +0800

    1208

---
+++ MedSysProject/ViewModels/CPlanProjectItems.cs
@@ -0,0 +1,20 @@
+﻿namespace MedSysProject.ViewModels
+{
+    public class CPlanProjectItems
+    {
+       
+        public int PlanId { get; set; }
+
+        public string PlanName { get; set; }
+
+        public string PlanDescription { get; set; }
+        public int ProjectId { get; set; }
+
+        public string ProjectName { get; set; }
+        public double? ProjectPrice { get; set; }
+
+        public int ItemId { get; set; }
+
+        public string ItemName { get; set; }
+    }
+}
