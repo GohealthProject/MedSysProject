dd5fdad9be7cf73a3dd58c1ffeb47474c6423419
SteveisComing <emile814034@gmail.com>
2023-12-09 23:46:54 +0800

    電腦死了，新電腦第一推

--- MedSysProject/Controllers/BlogsController.cs
+++ MedSysProject/Controllers/BlogsController.cs
@@ -227,10 +227,10 @@ namespace MedSysProject.Controllers
             return PartialView(q);
         }
         public IActionResult ShowComments(int BlogId)
-        {//傳送到partial view的時候就要傳全部，然後在partialview中用razor去做分流
+        {
             var mainComments = (_db.Comments.Include(comment => comment.Member)
                                             .Include(comment => comment.Employee)
-                                            .Where(comment => comment.BlogId == BlogId)
+                                            .Where(comment => comment.BlogId == BlogId&&comment.ParentCommentId==null)
                                 .Select(comment => comment)).ToList();
             return PartialView(mainComments);
         }
