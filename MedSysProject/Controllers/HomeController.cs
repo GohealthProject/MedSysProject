0d935469134dad98666438d16ce848ab48f4b8ff
waynewang <waynewang1990@hotmail.com>
2023-12-12 00:43:56 +0800

    .

--- MedSysProject/Controllers/HomeController.cs
+++ MedSysProject/Controllers/HomeController.cs
@@ -131,7 +131,7 @@ namespace MedSysProject.Controllers
             return View();
         }
 
-        [HttpPost]
+        //[HttpPost]
         public IActionResult Reserve(IFormCollection item)
         { //預約總覽
 
