1b9b11966cc7132a7c5b5544191899b1ddb26689
Jacky Jie <jakcy2222@gmail.com>
2023-12-15 15:34:38 +0800

    aaaa

---
+++ MedSysProject/Controllers/TestAdminController.cs
@@ -0,0 +1,48 @@
+﻿using MedSysProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedSysProject.Controllers
+{
+    public class TestAdminController : Controller
+    {
+        //注入
+        private readonly MedSysContext  _db;
+        public TestAdminController(MedSysContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Member()
+        {
+            return View();
+        }
+
+        //Member Json
+        public IActionResult MemberJson()
+        {
+            return Json(_db.Members);
+        }
+
+        public IActionResult Employee()
+        {
+            return View();
+        }
+
+        public IActionResult Plan()
+        {
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+        
+    }
+}
