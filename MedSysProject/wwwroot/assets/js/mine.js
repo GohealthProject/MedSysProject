d7159c116211a48667d760ac4e2861923930112f
SteveisComing <emile814034@gmail.com>
2023-12-17 17:12:41 +0800

    QueryByKeyword

--- MedSysProject/wwwroot/assets/js/mine.js
+++ MedSysProject/wwwroot/assets/js/mine.js
@@ -88,11 +88,18 @@
     console.log(searchOpen);//抓到了
     const searchClose = document.querySelector('.js-search-close');
     const searchWrap = document.querySelector(".js-search-form-wrap");
-
+    const searchForm = document.querySelector('.search-form');
     searchOpen.addEventListener("click", (e) => {
         e.preventDefault();
         console.log("Hi");
         searchWrap.classList.add("active");
+        searchForm.addEventListener('keydown', function (e) {
+            if (e.key === 'Enter')
+            {
+                e.preventDefault();
+                searchForm.submit();
+            }
+        });
     });
 
     searchClose.addEventListener("click", (e) => {
