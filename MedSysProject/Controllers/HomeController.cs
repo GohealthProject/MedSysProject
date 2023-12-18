96985e1ffbba231a1b320dea1c9505805ac30193
wayne <waynewang1990@hotmail.com>
2023-12-18 18:17:12 +0800

    .

--- MedSysProject/Controllers/HomeController.cs
+++ MedSysProject/Controllers/HomeController.cs
@@ -389,7 +389,7 @@ namespace MedSysProject.Controllers
             ViewBag.item = item["item"];
             ViewBag.mid = item["Mid"];
             ViewBag.pid = item["Pid"];
-            ViewBag.member = HttpContext.Session.Get(CDictionary.SK_PLAN_COMPARERE_RESULT);
+            ViewBag.member = HttpContext.Session.Get(CDictionary.SK_MEMBER_LOGIN);
             //ViewBag.test = "test123";
             //ViewBag.item = item;
 
