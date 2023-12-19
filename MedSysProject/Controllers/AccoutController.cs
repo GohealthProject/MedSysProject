8c9a2033d867f043c195bd4d94b2054878c3b47c
Jacky Jie <jakcy2222@gmail.com>
2023-12-20 01:40:45 +0800

    Google登入改好了

--- MedSysProject/Controllers/AccoutController.cs
+++ MedSysProject/Controllers/AccoutController.cs
@@ -429,52 +429,52 @@ namespace MedSysProject.Controllers
         /// 驗證 Google 登入授權
         /// </summary>
         /// <returns></returns>
-        public IActionResult ValidGoogleLogin()
-        {
-            string? formCredential = Request.Form["credential"]; //回傳憑證
-            string? formToken = Request.Form["g_csrf_token"]; //回傳令牌
-            string? cookiesToken = Request.Cookies["g_csrf_token"]; //Cookie 令牌
+        //public IActionResult ValidGoogleLogin()
+        //{
+        //    string? formCredential = Request.Form["credential"]; //回傳憑證
+        //    string? formToken = Request.Form["g_csrf_token"]; //回傳令牌
+        //    string? cookiesToken = Request.Cookies["g_csrf_token"]; //Cookie 令牌
 
-            // 驗證 Google Token
-            GoogleJsonWebSignature.Payload? payload = VerifyGoogleToken(formCredential, formToken, cookiesToken).Result;
-            if (payload == null)
-            {
-                // 驗證失敗
-                ViewData["Msg"] = "驗證 Google 授權失敗";
-            }
-            else
-            {
-                IEnumerable<Member> q = null;
+        //    // 驗證 Google Token
+        //    GoogleJsonWebSignature.Payload? payload = VerifyGoogleToken(formCredential, formToken, cookiesToken).Result;
+        //    if (payload == null)
+        //    {
+        //        // 驗證失敗
+        //        ViewData["Msg"] = "驗證 Google 授權失敗";
+        //    }
+        //    else
+        //    {
+        //        IEnumerable<Member> q = null;
 
-                if (_db.Members.FirstOrDefault(n => n.MemberEmail == payload.Email) == null)
-                {
-                    TempData["GAccountName"] = payload.Name.ToString();
-                    TempData["GAccountEmail"] = payload.Email.ToString();
+        //        if (_db.Members.FirstOrDefault(n => n.MemberEmail == payload.Email) == null)
+        //        {
+        //            TempData["GAccountName"] = payload.Name.ToString();
+        //            TempData["GAccountEmail"] = payload.Email.ToString();
 
-                    return RedirectToAction("Register");
-                }
-                else
-                {
-                    var qa = _db.Members.FirstOrDefault(n => n.MemberEmail == payload.Email);
-                    string json = JsonSerializer.Serialize(qa);
-                    HttpContext.Session.SetString(CDictionary.SK_MEMBER_LOGIN, json);
+        //            return RedirectToAction("Register");
+        //        }
+        //        else
+        //        {
+        //            var qa = _db.Members.FirstOrDefault(n => n.MemberEmail == payload.Email);
+        //            string json = JsonSerializer.Serialize(qa);
+        //            HttpContext.Session.SetString(CDictionary.SK_MEMBER_LOGIN, json);
 
-                    return RedirectToAction("MemberCenter", "Accout");
-                }
-
-
-
-                //驗證成功，取使用者資訊內容
-                ViewData["Msg"] = "驗證 Google 授權成功" + "<br>";
-                ViewData["Msg"] += "Email:" + payload.Email + "<br>";
-                ViewData["Msg"] += "Name:" + payload.Name + "<br>";
-                ViewData["Msg"] += "Picture:" + payload.Picture;
-            }
+        //            return RedirectToAction("MemberCenter", "Accout");
+        //        }
 
 
 
-            return View();
-        }
+        //        //驗證成功，取使用者資訊內容
+        //        ViewData["Msg"] = "驗證 Google 授權成功" + "<br>";
+        //        ViewData["Msg"] += "Email:" + payload.Email + "<br>";
+        //        ViewData["Msg"] += "Name:" + payload.Name + "<br>";
+        //        ViewData["Msg"] += "Picture:" + payload.Picture;
+        //    }
+
+
+
+        //    return View();
+        //}
 
         /// <summary>
         /// 驗證 Google Token
@@ -483,55 +483,55 @@ namespace MedSysProject.Controllers
         /// <param name="formToken"></param>
         /// <param name="cookiesToken"></param>
         /// <returns></returns>
-        public async Task<GoogleJsonWebSignature.Payload?> VerifyGoogleToken(string? formCredential, string? formToken, string? cookiesToken)
-        {
-            // 檢查空值
-            if (formCredential == null || formToken == null && cookiesToken == null)
-            {
-                return null;
-            }
+        //public async Task<GoogleJsonWebSignature.Payload?> VerifyGoogleToken(string? formCredential, string? formToken, string? cookiesToken)
+        //{
+        //    // 檢查空值
+        //    if (formCredential == null || formToken == null && cookiesToken == null)
+        //    {
+        //        return null;
+        //    }
 
-            GoogleJsonWebSignature.Payload? payload;
-            try
-            {
-                // 驗證 token
-                if (formToken != cookiesToken)
-                {
-                    return null;
-                }
+        //    GoogleJsonWebSignature.Payload? payload;
+        //    try
+        //    {
+        //        // 驗證 token
+        //        if (formToken != cookiesToken)
+        //        {
+        //            return null;
+        //        }
 
-                // 驗證憑證
-                IConfiguration Config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
-                string GoogleApiClientId = Config.GetSection("GoogleApiClientId").Value;
-                var settings = new GoogleJsonWebSignature.ValidationSettings()
-                {
-                    Audience = new List<string>() { GoogleApiClientId }
-                };
-                payload = await GoogleJsonWebSignature.ValidateAsync(formCredential, settings);
-                if (!payload.Issuer.Equals("accounts.google.com") && !payload.Issuer.Equals("https://accounts.google.com"))
-                {
-                    return null;
-                }
-                if (payload.ExpirationTimeSeconds == null)
-                {
-                    return null;
-                }
-                else
-                {
-                    DateTime now = DateTime.Now.ToUniversalTime();
-                    DateTime expiration = DateTimeOffset.FromUnixTimeSeconds((long)payload.ExpirationTimeSeconds).DateTime;
-                    if (now > expiration)
-                    {
-                        return null;
-                    }
-                }
-            }
-            catch
-            {
-                return null;
-            }
-            return payload;
-        }
+        //        // 驗證憑證
+        //        IConfiguration Config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+        //        string GoogleApiClientId = Config.GetSection("GoogleApiClientId").Value;
+        //        var settings = new GoogleJsonWebSignature.ValidationSettings()
+        //        {
+        //            Audience = new List<string>() { GoogleApiClientId }
+        //        };
+        //        payload = await GoogleJsonWebSignature.ValidateAsync(formCredential, settings);
+        //        if (!payload.Issuer.Equals("accounts.google.com") && !payload.Issuer.Equals("https://accounts.google.com"))
+        //        {
+        //            return null;
+        //        }
+        //        if (payload.ExpirationTimeSeconds == null)
+        //        {
+        //            return null;
+        //        }
+        //        else
+        //        {
+        //            DateTime now = DateTime.Now.ToUniversalTime();
+        //            DateTime expiration = DateTimeOffset.FromUnixTimeSeconds((long)payload.ExpirationTimeSeconds).DateTime;
+        //            if (now > expiration)
+        //            {
+        //                return null;
+        //            }
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        return null;
+        //    }
+        //    return payload;
+        //}
 
         public async Task googlelogin()
         {
@@ -551,18 +551,32 @@ namespace MedSysProject.Controllers
 
             var claims = result.Principal.Identities.FirstOrDefault()?.Claims.Select(claim => new
             {
-                claim.Issuer,
-                claim.OriginalIssuer,
-                claim.Type,
+                //claim.Issuer,
+                //claim.OriginalIssuer,
+                //claim.Type,
                 claim.Value
             });
 
-            //string json = JsonSerializer.Serialize(claims);
-            //HttpContext.Session.SetString(CDictionary.SK_MEMBER_LOGIN, json);
+            GAccountName = claims.ElementAt(1).Value;
+            GAccountEmail = claims.ElementAt(4).Value;
 
-            //return RedirectToAction("MemberCenter", "Accout");
+            if (_db.Members.FirstOrDefault(n => n.MemberEmail == GAccountEmail) == null)
+            {
+                TempData["GAccountName"] = GAccountName;
+                TempData["GAccountEmail"] = GAccountEmail;
 
-            return Json(claims);
+                return RedirectToAction("Register");
+            }
+            else
+            {
+                var qa = _db.Members.FirstOrDefault(n => n.MemberEmail == GAccountEmail);
+                string json = JsonSerializer.Serialize(qa);
+                HttpContext.Session.SetString(CDictionary.SK_MEMBER_LOGIN, json);
+
+                return RedirectToAction("MemberCenter", "Accout");
+            }
+
+            //return Json(claims);
         }
 
         public IActionResult MenberQA()
